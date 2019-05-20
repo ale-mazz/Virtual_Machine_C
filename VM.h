d943a6d4bd73b31aa5b37be9fe3aa9e636a63788
DavideVio <33291271+DavideVio@users.noreply.github.com>
2019-05-20 14:39:51 +0200

    Update VM.h

--- VM.h
+++ VM.h
@@ -1,10 +1,12 @@
 #define MEMSIZE (1<<16)
+
 struct virtual_machine {
     unsigned char stack[MEMSIZE];
     int reg[32];
-    unsigned int sp=0;
-    unsigned int ip=0;
-}
+    unsigned int sp;
+    unsigned int ip;
+};
 typedef struct virtual_machine VM;
-void print_input (FILE *fp);
+
+void stampa_input (FILE *fp);
 int execute (FILE *fp, VM mem);
