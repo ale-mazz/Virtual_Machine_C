bf4a3b81a4cfb8ddac35994ddafb2a55f49a7f83
DavideVio <33291271+DavideVio@users.noreply.github.com>
2019-05-20 15:37:39 +0200

    Add files via upload
    Ora stampa correttamente solo i numeri. Bisogna inserire i vari casi con lo switch.

--- print.c
+++ print.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include "VM.h"
 /*
  * Funzione di stampa del file input come riportato da specifiche di progetto
 */
 
 void print_input(FILE *fp){
-
-    char line[2];
-
-    while(fgets(line,2,fp)){
-        printf("%s",line);
+    /*char line[2];*/
+    char *line;
+    size_t linesize=1;
+    ssize_t nchar;
+    line=(char*)malloc (sizeof (char)*linesize);
+    while((nchar=getline (&line,&linesize, fp))!=-1){
+        char *p=strtok (line," ;");
+        if (p[0]>='0' && p[0]<='9')
+            printf("%s\n",p);
     }
+    free (line);
+    /*while (fgets(line,3,fp)){
+        if (line[0]>='0' && line[0]<='9')
+            printf ("%s\n",line);
+    }*/
     printf ("File created\n");
 }
