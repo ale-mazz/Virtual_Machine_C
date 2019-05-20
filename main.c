d2e7b487167f56e225a69248dc14bac1e9dffcd0
ale <broodyblack@hotmail.it>
2019-05-20 11:47:11 +0200

    Funzione di print

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include "print.c"
+#include "execute.c"
 
 int main() {
 
-    /* In questo main dobbiamo poter eseguire due operazioni:
+    /*
+     * In questo main dobbiamo poter eseguire due operazioni:
      * print : Stampa il file di input come una serie di comandi
      * execute : Esegue i comandi riportarti all'interno del file input
      */
-
+    FILE *fp;
+    fp = fopen ("Fattoriale_prova.txt","r");
+    print_input(fp);
     return 0;
 }
--- print.c
+++ print.c
@@ -3,3 +3,18 @@
 /*
  * Funzione di stampa del file input come riportato da specifiche di progetto
 */
+
+void print_input(FILE *fp){
+
+    char line[2];
+    if (fp == NULL) {
+        printf ("File not created\n");
+        return;
+    }
+
+    while(fgets(line,2,fp)){
+        printf("%s",line);
+    }
+    fclose (fp);
+    printf ("File created\n");
+}
