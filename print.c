dd09349a90eeb1bfb58f111923d7577e5013ee4c
DavideVio <33291271+DavideVio@users.noreply.github.com>
2019-05-20 14:44:59 +0200

    Update print.c
    Spostato nel main il codice inerente alla gestione del file

--- print.c
+++ print.c
@@ -7,14 +7,9 @@
 void print_input(FILE *fp){
 
     char line[2];
-    if (fp == NULL) {
-        printf ("File not created\n");
-        return;
-    }
 
     while(fgets(line,2,fp)){
         printf("%s",line);
     }
-    fclose (fp);
     printf ("File created\n");
 }
