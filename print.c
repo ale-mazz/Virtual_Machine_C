2f495329a46f7a0a3957249b0e23867aacc65ecf
Alessandro Mazzon <broodyblack@hotmail.it>
2019-05-21 19:43:23 +0200

    Print with some bugs

--- print.c
+++ print.c
@@ -22,82 +22,190 @@ void print_input(FILE *fp) {
         if (p[0] >= '0' && p[0] <= '9') {
             count_line++;
 
-            if(!strcmp(p,"12")){
-                printf("[ %d]  MOV ",count_line);
+            if(!strcmp(p,"0")){
+                printf("[%3d]  HALT \n",count_line);
+            }
+
+            else if(!strcmp(p,"1")){
+                printf("[%3d]  DISPLAY ",count_line);
+                count_reg = 1;
+                while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
+
+                    char *reg = strtok(line, " ;");
+                    count_line++;
+                    if(count_reg == 1){
+                        printf("R%s \n",reg);
+                        count_line++;
+                        count_reg--;
+                    }
+                }
+            }
+
+            else if(!strcmp(p,"10")){
+                printf("[%3d]  PUSH ",count_line);
+                count_reg = 1;
+                while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
+                    char *reg = strtok(line, " ;");
+                    if(count_reg == 1){
+                        printf("R%s ",reg);
+                        count_line++;
+                        count_reg--;
+                    }
+                }
+            }
+
+            else if(!strcmp(p,"12")){
+                printf("[%3d]  MOV ",count_line);
                 count_reg = 2;
-                while ((getline(&line, &linesize, fp)) != -1 && count_reg){
+                while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
                     if(count_reg == 2){
                         printf("R%s ",reg);
                         count_line++;
                         count_reg--;
                     }
-                    else{
+                    else if(count_reg == 1){
                         printf("%s\n",reg);
                         count_line++;
                         count_reg--;
                     }
                 }
-
             }
-
-            if(!strcmp(p,"12")){
-                printf("[ %d]  MOV ",count_line);
+            else if(!strcmp(p,"20")){
+                printf("[%3d]  CALL ",count_line);
+                count_reg = 1;
+                while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
+                    char *reg = strtok(line, " ;");
+                    if(count_reg == 1){
+                        printf("%s\n",reg);
+                        count_line++;
+                        count_reg--;
+                    }
+                }
+            }
+            else if(!strcmp(p,"21")){
+                printf("[%3d]  RET \n",count_line);
+            }
+            else if(!strcmp(p,"22")){
+                printf("[%3d]  JMP ",count_line);
+                count_reg = 1;
+                while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
+                    char *reg = strtok(line, " ;");
+                    if(count_reg == 1){
+                        printf("%s\n",reg);
+                        count_line++;
+                        count_reg--;
+                    }
+                }
+            }
+            else if(!strcmp(p,"23")){
+                printf("[%3d]  JZ ",count_line);
+                count_reg = 1;
+                while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
+                    char *reg = strtok(line, " ;");
+                    if(count_reg == 1){
+                        printf("%s\n",reg);
+                        count_line++;
+                        count_reg--;
+                    }
+                }
+            }
+            else if(!strcmp(p,"24")){
+                printf("[%3d]  JPOS ",count_line);
+                count_reg = 1;
+                while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
+                    char *reg = strtok(line, " ;");
+                    if(count_reg == 1){
+                        printf("%s\n",reg);
+                        count_line++;
+                        count_reg--;
+                    }
+                }
+            }
+            else if(!strcmp(p,"25")){
+                printf("[%3d]  JNEG ",count_line);
+                count_reg = 1;
+                while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
+                    char *reg = strtok(line, " ;");
+                    if(count_reg == 1){
+                        printf("%s\n",reg);
+                        count_line++;
+                        count_reg--;
+                    }
+                }
+            }
+            else if(!strcmp(p,"30")){
+                printf("[%3d]  ADD ",count_line);
                 count_reg = 2;
-                while ((getline(&line, &linesize, fp)) != -1 && count_reg){
+                while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
                     if(count_reg == 2){
                         printf("R%s ",reg);
                         count_line++;
                         count_reg--;
                     }
-                    else{
-                        printf("%s\n",reg);
+                    else if(count_reg == 1){
+                        printf("R%s\n",reg);
                         count_line++;
                         count_reg--;
                     }
                 }
-
             }
-
-            if(!strcmp(p,"12")){
-                printf("[ %d]  MOV ",count_line);
+            else if(!strcmp(p,"31")){
+                printf("[%3d]  SUB ",count_line);
                 count_reg = 2;
-                while ((getline(&line, &linesize, fp)) != -1 && count_reg){
+                while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
                     if(count_reg == 2){
                         printf("R%s ",reg);
                         count_line++;
                         count_reg--;
                     }
-                    else{
-                        printf("%s\n",reg);
+                    else if(count_reg == 1){
+                        printf("R%s\n",reg);
                         count_line++;
                         count_reg--;
                     }
                 }
-
             }
-
-            if(!strcmp(p,"12")) {
-                printf("[ %d]  MOV ", count_line);
+            else if(!strcmp(p,"32")){
+                printf("[%3d]  MUL ",count_line);
                 count_reg = 2;
-                while ((getline(&line, &linesize, fp)) != -1 && count_reg) {
+                while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
-                    if (count_reg == 2) {
-                        printf("R%s ", reg);
+                    if(count_reg == 2){
+                        printf("R%s ",reg);
                         count_line++;
                         count_reg--;
-                    } else {
-                        printf("%s\n", reg);
+                    }
+                    else if(count_reg == 1){
+                        printf("R%s\n",reg);
                         count_line++;
                         count_reg--;
                     }
                 }
-
             }
+            else if(!strcmp(p,"33")){
+                printf("[%3d]  DIV ",count_line);
+                count_reg = 2;
+                while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
+                    char *reg = strtok(line, " ;");
+                    if(count_reg == 2){
+                        printf("R%s ",reg);
+                        count_line++;
+                        count_reg--;
+                    }
+                    else if(count_reg == 1){
+                        printf("R%s\n",reg);
+                        count_line++;
+                        count_reg--;
+                    }
+                }
+            }
+
+        }else{
+            count_line--;
         }
     }
     free(line);
-    printf("File created\n");
 }
