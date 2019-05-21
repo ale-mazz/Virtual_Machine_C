a58957f4154cf04ec9637ed07d36dcc17cb87405
DavideVio <33291271+DavideVio@users.noreply.github.com>
2019-05-21 21:49:40 +0200

    Add files via upload
    Sistemato il problema

--- print.c
+++ print.c
@@ -16,23 +16,23 @@ void print_input(FILE *fp) {
     size_t linesize = 1;
     ssize_t nchar;
     line = (char *) malloc(sizeof(char) * linesize);
-    while ((getline(&line, &linesize, fp)) != -1) {
+    while ((nchar=getline(&line, &linesize, fp)) != -1) {
         char *p = strtok(line, " ;");
 
-        if (p[0] >= '0' && p[0] <= '9') {
-            count_line++;
-
+        if (p[0] >= '0' && p[0] <= '9'&& count_line>=0) {
+            
             if(!strcmp(p,"0")){
                 printf("[%3d]  HALT \n",count_line);
+                count_line++;
             }
 
             else if(!strcmp(p,"1")){
                 printf("[%3d]  DISPLAY ",count_line);
                 count_reg = 1;
+                count_line++;
                 while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
 
                     char *reg = strtok(line, " ;");
-                    count_line++;
                     if(count_reg == 1){
                         printf("R%s \n",reg);
                         count_line++;
@@ -44,6 +44,7 @@ void print_input(FILE *fp) {
             else if(!strcmp(p,"10")){
                 printf("[%3d]  PUSH ",count_line);
                 count_reg = 1;
+                count_line++;
                 while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
                     if(count_reg == 1){
@@ -53,27 +54,36 @@ void print_input(FILE *fp) {
                     }
                 }
             }
+            else if (!strcmp(p,"11")){
+                printf ("[%3d]  POP ",count_line);
+                count_reg=1;
+                count_line++;
+                while (count_reg!=0 && (getline (&line, &linesize, fp))!=-1){
+                    char *reg =strtok(line, " ;");
+                    printf ("R%s \n",reg);
+                    count_line++;
+                    count_reg--;
+                }
+            }
 
             else if(!strcmp(p,"12")){
                 printf("[%3d]  MOV ",count_line);
                 count_reg = 2;
+                count_line++;
                 while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
-                    if(count_reg == 2){
+                    if(count_reg == 2)
                         printf("R%s ",reg);
-                        count_line++;
-                        count_reg--;
-                    }
-                    else if(count_reg == 1){
+                    else 
                         printf("%s\n",reg);
-                        count_line++;
-                        count_reg--;
-                    }
+                    count_line++;
+                    count_reg--;
                 }
             }
             else if(!strcmp(p,"20")){
                 printf("[%3d]  CALL ",count_line);
                 count_reg = 1;
+                count_line++;
                 while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
                     if(count_reg == 1){
@@ -85,10 +95,12 @@ void print_input(FILE *fp) {
             }
             else if(!strcmp(p,"21")){
                 printf("[%3d]  RET \n",count_line);
+                count_line++;
             }
             else if(!strcmp(p,"22")){
                 printf("[%3d]  JMP ",count_line);
                 count_reg = 1;
+                count_line++;
                 while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
                     if(count_reg == 1){
@@ -101,6 +113,7 @@ void print_input(FILE *fp) {
             else if(!strcmp(p,"23")){
                 printf("[%3d]  JZ ",count_line);
                 count_reg = 1;
+                count_line++;
                 while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
                     if(count_reg == 1){
@@ -113,6 +126,7 @@ void print_input(FILE *fp) {
             else if(!strcmp(p,"24")){
                 printf("[%3d]  JPOS ",count_line);
                 count_reg = 1;
+                count_line++;
                 while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
                     if(count_reg == 1){
@@ -125,6 +139,7 @@ void print_input(FILE *fp) {
             else if(!strcmp(p,"25")){
                 printf("[%3d]  JNEG ",count_line);
                 count_reg = 1;
+                count_line++;
                 while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
                     if(count_reg == 1){
@@ -137,6 +152,7 @@ void print_input(FILE *fp) {
             else if(!strcmp(p,"30")){
                 printf("[%3d]  ADD ",count_line);
                 count_reg = 2;
+                count_line++;
                 while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
                     if(count_reg == 2){
@@ -154,6 +170,7 @@ void print_input(FILE *fp) {
             else if(!strcmp(p,"31")){
                 printf("[%3d]  SUB ",count_line);
                 count_reg = 2;
+                count_line++;
                 while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
                     if(count_reg == 2){
@@ -171,6 +188,7 @@ void print_input(FILE *fp) {
             else if(!strcmp(p,"32")){
                 printf("[%3d]  MUL ",count_line);
                 count_reg = 2;
+                count_line++;
                 while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
                     if(count_reg == 2){
@@ -188,6 +206,7 @@ void print_input(FILE *fp) {
             else if(!strcmp(p,"33")){
                 printf("[%3d]  DIV ",count_line);
                 count_reg = 2;
+                count_line++;
                 while (count_reg!=0 && (getline(&line, &linesize, fp)) != -1){
                     char *reg = strtok(line, " ;");
                     if(count_reg == 2){
@@ -203,9 +222,9 @@ void print_input(FILE *fp) {
                 }
             }
 
-        }else{
-            count_line--;
         }
+        if (count_line<0)
+            count_line++;
     }
     free(line);
 }
