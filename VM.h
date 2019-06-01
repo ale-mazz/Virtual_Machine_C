b8d1383142dc77a07758d054c1c553fb59803eb7
DavideVio <33291271+DavideVio@users.noreply.github.com>
2019-06-01 12:08:16 +0200

    Add files via upload
    Modificato VM.h: MEMSIZE è ora 1024*64 (non so se sia la stessa cosa di 1<<16), modificati i parametri delle varie funzioni (ora sono tutti int * e non int**).
    Modificato main: modificato in modo tale che possa eseguire la execute
    Modificato execute: non so se sia giusto ma purtroppo con il main che non mi va non posso testarlo. Da controllare attentamente (secondo me bisogna fare mem->ip+=2 e +=3

--- VM.h
+++ VM.h
@@ -1,16 +1,14 @@
-#define MEMSIZE (1<<16)
+#define MEMSIZE (sizeof(int)*1024*64)
 
 struct virtual_machine {
-    unsigned char stack[MEMSIZE];
+    int stack[MEMSIZE];
     int reg[32];
-    /*
     unsigned int sp;
     unsigned int ip;
-     */
+     
 };
 typedef struct virtual_machine VM;
 
 void print_assembly(int *input_array, int input_array_size);
-
-//int execute (FILE *fp, VM mem);
-int input_to_array(char *input_name, int **input_array);
+int execute (int *input_array,int input_array_size, VM *mem);
+int input_to_array(char *input_name, int *input_array);
--- main.c
+++ main.c
@@ -3,28 +3,33 @@
 #include <stdlib.h>
 #include "VM.h"
 
-/*#include "execute.c"*/
 int main(int argc, char *argv[]) {
 
     int *input_array = 0;
     char *input_name = NULL;
     char *instruction_name = NULL;
     int input_array_size = 0;
-
+    VM *virtual_stack;
+    virtual_stack=(VM*)malloc (sizeof (struct virtual_machine));
+    virtual_stack->sp=0;
+    virtual_stack->ip=0;
     instruction_name = argv[1];
     input_name = argv[2];
 
     if (strcmp(instruction_name, "print") == 0) {
         printf("Stampa\n");
-        input_array_size = input_to_array(input_name, &input_array);
+        input_array_size = input_to_array(input_name, input_array);
         print_assembly(input_array, input_array_size);
     } else if (strcmp(instruction_name, "execute") == 0) {
         printf("Esegui\n");
+        input_array_size=input_to_array(input_name, input_array);
+        execute (input_array, input_array_size, virtual_stack);
     } else {
         printf("Istruzione scorretta.\n");
     }
 
-
+    free (virtual_stack);
     free(input_array);
+    void (argc);
     return 0;
 }
